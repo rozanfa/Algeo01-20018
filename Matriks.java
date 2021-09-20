2a590465d0374c03b2bb0dd4c831d514a4b7c87d
rozanfa <16520141@std.stei.itb.ac.id>
2021-09-20 22:18:07 +0700

    Change matrix element type to float

--- Matriks.java
+++ Matriks.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Matriks {
     
-    int Mat[][] = new int[10][10];
+    float Mat[][] = new float[10][10];
 
     int iEff = 0;
     int jEff = 0;
@@ -22,7 +22,9 @@ public class Matriks {
     void isiMatriks(){
         Scanner scanner = new Scanner(System.in);
         int n, m;
+        System.out.print("Masukkan baris matriks: ");
         n = scanner.nextInt();
+        System.out.print("Masukkan kolom matriks: ");
         m = scanner.nextInt();
         
         int i,j;
@@ -44,6 +46,28 @@ public class Matriks {
                 System.out.print(this.Mat[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    void substractRow(int r, int s, float k){
+        // Baris r dikurangi k kali baris s
+        for (int j = 0; j < this.jEff; j++){
+            this.Mat[r][j] -= k *this.Mat[s][j];
+        }
+    }
+
+    void makeLeftOne(int r, int c){
+        // Menjadikan elemen baris paling kiri bernilai satu 
+        // dengan membagi baris dengan konstanta yang sesuai
+        float k = this.Mat[r][c];
+        for (int j = c; j < this.jEff; j++){
+            this.Mat[r][j] /= k;
+        }
+    }
+
+    void spl_gauss(){
+        if (this.Mat[0][0] != 1){
+            
         }
     }
 }
--- driverMatriks.java
+++ driverMatriks.java
@@ -3,6 +3,7 @@ public class driverMatriks {
     public static void main(String[] args) {
         Matriks M = new Matriks();
         M.isiMatriks();
+        M.makeLeftOne(0,0);
         M.cetakMatriks();
     }
 }
