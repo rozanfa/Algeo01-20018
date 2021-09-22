845dbcec1ef5ff2a5ea0867e58e9fb865fdaffda
rozanfa <16520141@std.stei.itb.ac.id>
2021-09-23 00:20:04 +0700

    Add matriks solusi banyak (aplha version)

--- src/SPL_Matriks.java
+++ src/SPL_Matriks.java
@@ -31,33 +31,78 @@ public class SPL_Matriks extends Matriks{
 
 
 
-    /*
+    
     void printManySolution(){
-        Char var = 'r';
-        List res = new List();
-        Int p = this.iEff - 1;
+        char var[] = {'r','s','t','u','v','w','x','y','z','a'};
+        int maxVar = this.jEff - this.iEff + 1;
+        Float res[] = new Float[10];
+        boolean isResConstant[] = new boolean[10];
+        String stringRes[] = new String[10];
+        int p = this.jEff - 2;
+        int j = 0;
+        
         for (int i=0; i<this.iEff; i++){
-            res[i] = 0;
-            if (this.Mat[i][i] != 1){
-                var++;
+            res[i] = (float) 0;
+            while (this.Mat[i][j] != 1 && j < jEff - 2){
+                j++;
+                maxVar++;
             }
         }
+        int countVar = maxVar;
         for (int i=this.iEff; i>=0; i--){
             if (this.Mat[i][p] == 1){
                 res[p] = this.Mat[i][this.jEff-1];
+                if (maxVar == 0){
+                    isResConstant[p] = true;
+                }
+                else {
+                    isResConstant[p] = true;
+                    for (j = this.jEff - 2; j > p; j--){
+                        stringRes[j] = "";
+                        if (isResConstant[j]) {
+                            res[p] -= res[j];
+                        }
+                        else {
+                            isResConstant[p] = false;
+                            System.out.println(countVar);
+                            stringRes[j] += String.format("%.2f", this.Mat[i][j] * - 1) + var[countVar];
+                            countVar--;
+                        }
+                        stringRes[j] = res[p].toString() + "+(" + stringRes[j] + ")";
+                    }
+                }
                 p--;
+                
             }
             else {
                 while (p > 0 && this.Mat[i][p] != 1){
-                    res[p] = var;
-                    var--;
+                    isResConstant[p] = false;
+                    stringRes[p] = "" + var[countVar];
+                    countVar--;
                     p--;
                 }
             }
         }
-
+        for (j=0; j < this.jEff - 1; j++){
+            if (j != this.jEff - 2){
+                if (isResConstant[j]) {
+                    System.out.print(String.format("x[%d] = %.2f, ", j+1, res[j]));
+                }
+                else {
+                    System.out.print(String.format("x[%d] = " + stringRes[j] + " , ", j + 1));
+                }
+            }
+            else{
+                if (isResConstant[j]) {
+                    System.out.print(String.format("x[%d] = %.2f", j+1, res[j]));
+                }
+                else {
+                    System.out.print(String.format("x[%d] = " + stringRes[j], j + 1));
+                }
+            }
+        }
     }
-    */
+    
 
 
     void printUniqueSolution_gauss(){
@@ -109,7 +154,8 @@ public class SPL_Matriks extends Matriks{
         System.out.println("");
         if (isSPLHaveSolution()){
             if (isSPLHaveManySolution()){
-                System.out.println("Solusi banyak");
+                //System.out.println("Solusi banyak");
+                printManySolution();
             }
             else {
                 printUniqueSolution_gauss_jordan();
