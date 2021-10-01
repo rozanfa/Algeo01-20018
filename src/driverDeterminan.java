9bfedb739d997d38340089a5b0adcff0000d2a7d
barizahaqi <haqibariza@gmail.com>
2021-10-01 11:04:32 +0700

    revisi determinan 4.0

--- src/driverDeterminan.java
+++ src/driverDeterminan.java
@@ -2,10 +2,10 @@ public class driverDeterminan {
 	public static String metode_kofaktor(){
         String result = "Matriks yang akan dicari determinannya melalui metode kofaktor:\n";
         Matriks matriks = new Matriks();
-        Extended.readMatrix(matriks);
+        matriks.isiMatriks();
         System.out.println("\nMatriks yang dimasukkan:");
-        Extended.printMatrix(matriks);
-        result += Extended.getMatriksString(matriks) + "\n";
+        matriks.cetakMatriks();
+        result += matriks.getMatriksString() + "\n";
         result += "Determinan matriksnya adalah: ";
         System.out.println("Determinan matriksnya adalah: " + String.format("%.2f", Determinan_Matriks.kofaktor(matriks)));
         result += String.format("%.2f", Determinan_Matriks.kofaktor(matriks));
@@ -14,22 +14,22 @@ public class driverDeterminan {
 	public static String metode_sarrus() {
 		String result = "Matriks yang akan dicari determinannya melalui metode sarrus:\n";
         Matriks matriks = new Matriks();
-        Extended.readMatrix(matriks);
+        matriks.isiMatriks();
         System.out.println("\nMatriks yang dimasukkan:");
-        Extended.printMatrix(matriks);
+        matriks.cetakMatriks();
         Determinan_Matriks.DetwithSarrus(matriks);
-        result += Extended.getMatriksString(matriks) + "\n";
+        result += matriks.getMatriksString() + "\n";
         result+="Determinan matriksnya adalah: "+ String.format("%.2f", Determinan_Matriks.kofaktor(matriks));
         return result;
 	}
 	public static String metode_obe() {
 		String result = "Matriks yang akan dicari determinannya melalui metode obe:\n";
 		Matriks matriks = new Matriks();
-        Extended.readMatrix(matriks);
+		matriks.isiMatriks();
         System.out.println("\nMatriks yang dimasukkan:");
-        Extended.printMatrix(matriks);
+        matriks.cetakMatriks();
         Determinan_Matriks.DetwithOBE(matriks);
-        result += Extended.getMatriksString(matriks) + "\n";
+        result += matriks.getMatriksString()+"\n";
         result+="Determinan matriksnya adalah: "+ String.format("%.2f", Determinan_Matriks.kofaktor(matriks));
         return result;
 	}
