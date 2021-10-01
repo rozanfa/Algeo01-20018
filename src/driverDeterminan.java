da3071cd861c8d37fdee2b89d50bd2672919d2bf
barizahaqi <haqibariza@gmail.com>
2021-10-01 11:13:38 +0700

    revisi determinan 5.0

--- src/driverDeterminan.java
+++ src/driverDeterminan.java
@@ -12,15 +12,23 @@ public class driverDeterminan {
         return result;
     }
 	public static String metode_sarrus() {
-		String result = "Matriks yang akan dicari determinannya melalui metode sarrus:\n";
-        Matriks matriks = new Matriks();
-        matriks.isiMatriks();
-        System.out.println("\nMatriks yang dimasukkan:");
-        matriks.cetakMatriks();
-        Determinan_Matriks.DetwithSarrus(matriks);
-        result += matriks.getMatriksString() + "\n";
-        result+="Determinan matriksnya adalah: "+ String.format("%.2f", Determinan_Matriks.kofaktor(matriks));
-        return result;
+		public static String metode_sarrus() {
+			
+	        Matriks matriks = new Matriks();
+	        matriks.isiMatriks();
+	        System.out.println("\nMatriks yang dimasukkan:");
+	        matriks.cetakMatriks();
+	        Determinan_Matriks.DetwithSarrus(matriks);
+	        if (matriks.jEff==3 & matriks.iEff==3) {
+	        	String result = "Matriks yang akan dicari determinannya melalui metode sarrus:\n";
+	        	result += matriks.getMatriksString() + "\n";
+	            result+="Determinan matriksnya adalah: "+ String.format("%.2f", Determinan_Matriks.kofaktor(matriks));
+	            return result;
+	        }
+	        else {
+	        	String result ="Tidak bisa mencari determinan menggunakan metode Sarrus selain di matriks 3 x 3";
+	        	return result;
+	        }
 	}
 	public static String metode_obe() {
 		String result = "Matriks yang akan dicari determinannya melalui metode obe:\n";
